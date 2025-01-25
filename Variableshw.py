b793676b295da8ce6732bbdd8a6910a250e31aed
Relevant0421 <145288542+Relevant0421@users.noreply.github.com>
2025-01-24 20:17:47 -0500

    Add files via upload

---
+++ Variableshw.py
@@ -0,0 +1,43 @@
+print ("Multiplication of two numbers")
+value1 = int(input("Enter value 1: \n"))
+value2 = int(input("Enter value 2: \n"))
+
+Multiplication = value1 * value2
+print ("The multiplication result is: "+str(Multiplication))
+
+print ("\nDivision of two numbers")
+value1 = int(input("Enter value 1: \n"))
+value2 = int(input("Enter value 2: \n"))
+
+Division = value1 / value2
+print ("The division result is: "+str(Division))
+
+print ("\nDivision of two numbers but without remainder")
+value1 = int(input("Enter value 1: \n"))
+value2 = int(input("Enter value 2: \n"))
+
+Division_without_remainder = value1 // value2
+print ("The division result is: "+str(Division_without_remainder))
+
+print ("\nModulo of two numbers")
+value1 = int(input("Enter value 1: \n"))
+value2 = int(input("Enter value 2: \n"))
+
+Modulo = value1 % value2
+print ("The modulo result is: "+str(Modulo))
+
+print ("\nSum of two numbers")
+value1 = int(input("Enter value 1: \n"))
+value2 = int(input("Enter value 2: \n"))
+
+Sum = value1 + value2
+print ("The sum result is: "+str(Sum))
+
+print ("\nSubstraction of two numbers")
+value1 = int(input("Enter value 1: \n"))
+value2 = int(input("Enter value 2: \n"))
+
+Substract = value1 - value2
+print ("The substraction result is: "+str(Substract))
+
+
